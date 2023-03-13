62b4bf068d10ccff2d452fdaea20e7682acc944e
Enjere <artemmediv@gmail.com>
2023-03-13 23:24:06 +0300

    feat: delete done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+import fs from "fs/promises"
+import url from "url"
+
 const remove = async () => {
-    // Write your code here 
+    let dirname = url.fileURLToPath(new URL(".", import.meta.url))
+    await fs.unlink(`${dirname}files/fileToRemove.txt"`)
+        .catch(err => {
+            throw new Error("FS operation Failed");
+        })
 };
 
 await remove();
