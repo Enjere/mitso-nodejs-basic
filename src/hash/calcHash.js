57c138dcfb4a1dd227801a4715de1b38b6ee8e75
Enjere <artemmediv@gmail.com>
2023-03-13 23:28:18 +0300

    feat: calcHash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import { createHash } from "crypto"
+import fs from "fs/promises"
+import url from "url"
+
 const calculateHash = async () => {
-    // Write your code here 
+    let dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    let data = await fs.readFile(`${dirname}files/fileToCalculateHashFor.txt`);
+    console.log(createHash('sha256').update(data).digest("hex"));
 };
 
 await calculateHash();
