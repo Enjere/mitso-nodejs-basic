c9552b3e8860b0035de887290c944de34a23997b
Enjere <artemmediv@gmail.com>
2023-03-13 23:33:57 +0300

    feat: read done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from "fs"
+import {stdout, stderr} from "process"
+import url from "url"
+
 const read = async () => {
-    // Write your code here 
+    let dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    const streamIn = fs.createReadStream(`${dirname}files/fileToRead.txt`, "utf-8");
+    let data = "";
+    streamIn.on("data", chunk => data += chunk);
+    process.on("exit", code =>{
+        if(code === 0){
+            stdout.write(data);
+        }
+        else{
+            stderr.write("smt broke :(");
+        }
+    })
 };
 
 await read();
