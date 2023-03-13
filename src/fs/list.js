f71dd3ef7d2b98a842b36f52ef209b78908775f4
Enjere <artemmediv@gmail.com>
2023-03-13 23:24:58 +0300

    feat: list done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,11 @@
+import fs from "fs/promises"
+import url from "url"
+
 const list = async () => {
-    // Write your code here 
+    let dirname = url.fileURLToPath(new URL(".", import.meta.url))
+    let files = await fs.readdir(`${dirname}files`)
+        .catch((err) => {throw new Error("FS operation failed")});
+    console.log(files);
 };
 
 await list();
