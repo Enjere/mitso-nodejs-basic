e8a2db112c9998cd6e16f597ad246a5c2ad51530
Enjere <artemmediv@gmail.com>
2023-03-13 23:19:09 +0300

    feat: copy done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises"
+import url from "url"
+
 const copy = async () => {
-    // Write your code here 
+    let dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    let files = await fs.readdir(`${dirname}files`)
+        .catch((err) => {throw new Error("FS operation failed")});
+
+    let arr = new Array(files.length);
+    for(let i = 0; i < files.length; ++i){
+        arr[i] = await fs.readFile(`${dirname}files/${files[i]}`);
+    }
+    
+    await fs.mkdir("src/fs/files_copy")
+        .catch((err) => {throw new Error("FS operation failed")});
+    
+    for(let i = 0; i < files.length; ++i){
+        fs.writeFile(`${dirname}files_copy/${files[i]}`, arr[i]);
+    }
 };
 
 await copy();
